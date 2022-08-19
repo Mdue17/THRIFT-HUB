ef7ef9929640540953e85587c034bb32c8a9609e
Mdue17 <dubemdue99@gmail.com>
2022-08-19 22:41:56 +0200

    working build

--- routes/posts.js
+++ routes/posts.js
@@ -5,12 +5,23 @@ const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
-router.get("/:id", ensureAuth, postsController.getPost);
+
+router.get('/dashboard', ensureAuth, postController.dashboard)
+
+// router.get("/:id", ensureAuth, postsController.getPost);
 
 router.post("/createPost", upload.single("file"), postsController.createPost);
 
+router.delete('/deletePost', ensureAuth, postController.deletePost)
+
+router.get('/editButton/:id', ensureAuth, postController.editButton)
+
+router.put('/editPost/:id', ensureAuth, postController.editPost)
+
+router.get('/:id', ensureAuth, postController.getSinglePost)
+
 //router.put("/likePost/:id", postsController.likePost);
 
-router.delete("/deletePost/:id", postsController.markSold);
+// router.delete("/deletePost/:id", postsController.markSold);
 
 module.exports = router;
