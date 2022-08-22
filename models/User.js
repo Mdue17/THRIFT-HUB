667cb11a44a5b8e05c5f01f15538b95f1c22f5b3
Mdue17 <dubemdue99@gmail.com>
2022-08-22 22:38:33 +0200

    restructered

--- models/User.js
+++ models/User.js
@@ -3,31 +3,55 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  emailVerificationToken: String,
-  emailVerified: Boolean,
+  // email: { type: String, unique: true },
+  // password: String,
+  // passwordResetToken: String,
+  // passwordResetExpires: Date,
+  // emailVerificationToken: String,
+  // emailVerified: Boolean,
+  // google: String,
+  // instagram: String,
+ 
 
-  facebook: String,
-  twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
-  steam: String,
-  quickbooks: String,
-  tokens: Array,
+  // profile: {
+  //   name: String,
+  //   gender: String,
+  //   location: String,
+  //   website: String,
+  //   picture: String
+  // },
 
-  profile: {
-    name: String,
-    gender: String,
-    location: String,
-    website: String,
-    picture: String
-  }
-}, { timestamps: true });
+  loginID: {
+    type: String,
+    required: true,
+},
+firstName: {
+    type: String,
+    required: true,
+},
+lastName: {
+    type: String,
+    required: true,
+},
+displayName: {
+    type: String,
+    required: true,
+},
+email: {
+    type: String,
+    required: true,
+    trim: true,
+},
+image: {
+    type: String,
+},
+createdAt: {
+    type: Date,
+    default: Date.now,
+},
+},
+  
+ { timestamps: true });
 
 /**
  * Password hash middleware.
@@ -54,19 +78,7 @@ userSchema.methods.comparePassword = async function comparePassword(candidatePas
   }
 };
 
-/**
- * Helper method for getting user's gravatar.
- */
-userSchema.methods.gravatar = function gravatar(size) {
-  if (!size) {
-    size = 200;
-  }
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
+
 
 const User = mongoose.model('User', userSchema);
 
