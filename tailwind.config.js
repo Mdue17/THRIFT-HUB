6eb29f48b15caf715f9374cde6f452f3e40eb028
Mdue17 <dubemdue99@gmail.com>
2022-10-27 19:38:57 +0200

    config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+ /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./public/**/*.{html,js}",
+      "./views/*.ejs",
+   
+   
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require("daisyui")
+    
+    ],
+  
+   
+    darkMode: 'class',
+  
+  
+  }
